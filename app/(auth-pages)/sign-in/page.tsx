b14d031ecca0bf8c1d0c7f84332bbad1784f3c95
Hatim Shakir <hatimshakir9179@gmail.com>
2025-06-29 17:38:42 +0530

    fix: auth pages layout

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -9,7 +9,7 @@ import { Label } from "@/components/ui/label";
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <form className="flex-1 flex flex-col min-w-64">
+    <form className="flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
